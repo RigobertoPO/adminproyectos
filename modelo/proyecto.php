28e008d696350eda7d67a9a9233c8f8fb45dcdaa
rigobertopo <rperez@cobach.edu.mx>
2024-04-02 12:15:09 -0600

    confinguracion inicial MVC

---
+++ modelo/proyecto.php
@@ -0,0 +1,11 @@
+<?php
+class Proyecto{
+    private $listaproyectos;
+    public function __construct(){
+        $this->listaproyectos=array();
+    }
+    public function obtenerDatos($tabla,$condicion){
+
+    }
+}
+?>
