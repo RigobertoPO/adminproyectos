18deed737a7afba9e524ed0a7ae74c9e52575d84
rigobertopo <rperez@cobach.edu.mx>
2024-04-04 18:51:38 -0600

    Se generan los metodos de proyecto

--- modelo/proyecto.php
+++ modelo/proyecto.php
@@ -4,7 +4,50 @@ class Proyecto{
     public function __construct(){
         $this->listaproyectos=array();
     }
-    public function obtenerDatos($tabla,$condicion){
+    public function ObtenerDatos(){
+        include_once('conexion.php');
+        $cnn=new Conexion();
+        $consulta="SELECT * FROM proyecto";
+        $resultado=$cnn->prepare($consulta);
+        $resultado->execute();
+        while( $fila =$resultado->
+        fetchAll(PDO::FETCH_ASSOC)){
+            $this->listaproyectos[]=$fila;
+        }
+        return $this->listaproyectos;
+    }
+    public function InsertarProyecto($datos){
+        include_once('conexion.php');
+        $cnn=new Conexion();
+        $consulta="INSERT INTO Proyectos(Id,Clave,
+        Nombre,Caratula,FechaCreacion,IdUsuario)
+         VALUES(null,".$datos.")";
+        $resultado=$cnn->prepare( $consulta);
+        $resultado->execute();
+        if($resultado){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function ActualizarProyecto($datos,$condicion){
+        include_once('conexion.php');
+        $cnn=new Conexion();
+        $consulta="UPDATE proyectos SET 
+        ".$datos." WHERE ".$condicion;
+        $resultado=$cnn->prepare($consulta);
+        $resultado->execute();
+        if($resultado){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+    public function EliminarProyecto($condicion){
+
+    }
+    public function ObtenerProyectoId($condicion){
 
     }
 }
