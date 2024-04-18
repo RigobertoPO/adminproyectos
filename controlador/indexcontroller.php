a7f016d4e6db4f81acbd785307930fab91082b16
rigobertopo <rperez@cobach.edu.mx>
2024-04-17 20:54:07 -0600

    se agregan metodos de index controller

--- controlador/indexcontroller.php
+++ controlador/indexcontroller.php
@@ -1,6 +1,30 @@
 <?php
 require_once("modelo/index.php");
 class indexController{
-
+    function __construct(){
+    }
+    public static function index(){
+        require_once("vista/index.php");
+    }
+    public static function login(){
+        require_once("vista/login.php");
+    }
+    public static function registrar(){
+        require_once("vista/registro.php");
+    }
+    public static function autentificar(){
+        $correo=$_REQUEST['correo'];
+        $password=$_REQUEST['pass'];
+        $condicion="Correo='".$correo."' and Password='".$password."'";
+        $usuario=new Index();
+        $usuarioLogueado=$usuario->AutentificarUsuario($condicion);
+        require_once("vista/index.php");
+    }
+    public static function cerrarSesion(){
+        session_start();
+        if(session_destroy()){
+            require_once("vista/index.php"); 
+        }
+    }
 }
 ?>
---
+++ vista/index.php
@@ -0,0 +1,5 @@
+<?php
+require "vista/layout/header.php";
+echo '<h1>Bienvenido</h1>';
+require "vista/layout/footer.php";
+?>
